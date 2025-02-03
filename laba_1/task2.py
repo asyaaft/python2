0f77d30023dc1dcd2c83fe620b454aef948bb964
asyaaft <nastya17395@icloud.com>
2025-02-03 21:11:15 +0300

    Update task2.py

--- laba_1/task2.py
+++ laba_1/task2.py
@@ -1,6 +1,4 @@
-from task_1 import Book
-from task_1 import Tree
-from task_1 import Profile
+from task_1 import Book, Tree, Profile
 # TODO: импортируйте классы, созданные в ходе выполнения прошлого задания
 if __name__ == "__main__":
  # TODO: инстанцировать все описанные классы, создав три объекта.C()
@@ -12,16 +10,16 @@ if __name__ == "__main__":
      # TODO: вызвать метод с некорректными аргументами(b)
      book.read(2.4)
     except TypeError:
-        print('Ошибка: неправильные данные')
+     print('Ошибка: неправильные данные')
 
     try:
      # TODO: вызвать метод с некорректными аргументами(a)
      tree.grow("abc")
     except TypeError:
-        print('Ошибка: неправильные данные')
+     print('Ошибка: неправильные данные')
 
     try:
      # TODO: вызвать метод с некорректными аргументами(a)
      profile.add_friend(40.5)
     except TypeError:
-        print('Ошибка: неправильные данные')
+     print('Ошибка: неправильные данные')
